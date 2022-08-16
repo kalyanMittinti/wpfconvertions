dd8df12d4a7d53fd6b29636129ed570700a7ac6e
kalyan <66158468+kalyanMittinti@users.noreply.github.com>
2022-08-16 21:38:49 +0530

    updated new files
    updated data sucessfully

---
+++ Converter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+namespace WpfAppProject
+{
+    public class Converter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal unitPrice = 0;
+            decimal unitsinStock = 0;
+            unitPrice = (decimal)values[0];
+            string Netstock = string.Empty;
+             unitPrice = (values[0] != null && values[0] !=DependencyProperty.UnsetValue) ? System.Convert.ToDecimal(values[0]) :0;
+             unitsinStock = (values[0] != null && values[1] !=DependencyProperty.UnsetValue) ? System.Convert.ToDecimal(values[1]) :0;
+            unitPrice = (values[0] != null) ?
+System.Convert.ToDecimal(values[0]) : 0;
+            unitsinStock = (values[0] != null) ?
+System.Convert.ToDecimal(values[1]) : 0;
+            Netstock = System.Convert.ToString(unitPrice * unitsinStock);
+            return Netstock;
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targmetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        NorthwindEntities db=new NorthwindEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void mainloaded_Loaded(object sender, RoutedEventArgs e)
+        {
+            MyGrid.DataContext = GetAllProducts();
+        }
+        public List<Product> GetAllProducts()
+        {
+            var result = db.Products.Select(p => p).ToList();
+            return result;
+        }
+    }
+}
